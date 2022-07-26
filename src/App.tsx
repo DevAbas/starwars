3dd80b11e3ca8607586c8384cc943727878a8fe3
Abas <thurabli.abas@gmail.com>
2022-07-26 17:29:36 +0300

    Clean codes

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
 import Button from '@mui/material/Button'
-
-import logo from './logo.svg'
 import './App.css'
 
 const App = () => {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-        <Button variant='contained'>TEST MUI</Button>
-      </header>
-    </div>
-  )
+  return <div className='App'>Hello world</div>
 }
 
 export default App
